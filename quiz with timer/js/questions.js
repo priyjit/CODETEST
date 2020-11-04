b7448a675ce45066ba8aa0859920b6450d587033
priyjit <68781034+priyjit@users.noreply.github.com>
2020-11-04 11:13:44 +0530

    Add files via upload

---
+++ quiz with timer/js/questions.js
@@ -0,0 +1,68 @@
+let questions = [
+    {
+    numb: 1,
+    question: "ISP stands for?",
+    answer: "Internet Service Provider",
+    options: [
+      "intranet source provider",
+      "Intranet Service Providing",
+      "Internat Servoice Provider",
+      "Internet Service Provider"
+    ]
+  },
+    {
+    numb: 2,
+    question: "CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "which tag is use for link image?",
+    answer: "img",
+    options: [
+      "image",
+      "Hypertext Programming",
+      "img",
+      "imageos"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Www stand for?",
+    answer: "World Wide Web",
+    options: [
+      "world winde wab",
+      "World Wide Web",
+      "world wond web",
+      "ward wide web"
+    ]
+  },
+    {
+    numb: 5,
+    question: "name a popular verion control system?",
+    answer: "Git",
+    options: [
+      "Gat",
+      "Got",
+      "Gat",
+      "Git"
+    ]
+  },
+  {
+  numb: 6,
+  question: "which software is used to view web pages?",
+  answer: "Web Browser",
+  options: [
+    "Wab Browser",
+    "web blaster",
+    "Web Browser",
+    "Web Bing"
+  ]
+},
+];
